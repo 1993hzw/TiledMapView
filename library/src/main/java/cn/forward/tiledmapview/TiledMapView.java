141c701d8810a7438505bee97d6ab1a6d32c222f
huangziwei <huangziwei>
2019-04-26 11:13:38 +0800

    update license

--- library/src/main/java/cn/forward/tiledmapview/TiledMapView.java
+++ library/src/main/java/cn/forward/tiledmapview/TiledMapView.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2019  Ziwei Huang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cn.forward.tiledmapview;
 
 import android.animation.ValueAnimator;
@@ -27,6 +43,9 @@ import cn.forward.tiledmapview.core.MapRect;
 import cn.forward.tiledmapview.core.Tile;
 import cn.forward.tiledmapview.util.LogUtil;
 
+/**
+ * https://github.com/1993hzw/TiledMapView
+ */
 public class TiledMapView extends FrameLayout implements ITiledMapView, ILayer.Callback {
 
     private static boolean sDebugMode = false;
